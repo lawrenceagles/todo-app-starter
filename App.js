e22293b37dcef49eaf6d1bdd11f03822db5d6627
lawrenceagles <lawrenceagles@gmail.com>
2021-03-17 15:51:41 +0100

    finished starter boilerplate

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Keyboard, Dimensions, Platform } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import styled from "styled-components/native";
 
-const StyledView = styled.View`background-color: papayawhip;`;
+const screenHeight = Dimensions.get('window').height;
 
-const StyledText = styled.Text`color: palevioletred;`;
+const App = () => {
+  const [task, setTask] = useState(null);
+  const [tasks, setTasks] = useState([])
 
-export default function App () {
-	return (
-		<StyledView>
-			<StyledText>Hello World!</StyledText>
-		</StyledView>
-	);
+  const handleAddTask = () => {
+    if (!task) {
+      return;
+    }
+    Keyboard.dismiss();
+    setTasks([...tasks, task]) // using es6 spread operator.
+    setTask(null)
+  }
+
+  const handleDeleteTask = (taskIndex) => {
+    let currTask = [...tasks];
+    currTask.splice(taskIndex, 1);
+    setTasks(currTask);
+  }
+
+  return null;
 }
+
+export default App;
+
